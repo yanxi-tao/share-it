c7e91273c1ffd97cc021438ead677f457d1abba7
Yanxi-Tao <yanxi_tao@fis.edu>
2025-02-11 02:42:52 +0100

    some route tweaking, add form and simple space creation form setup

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,19 +1,10 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@/lib/devtools/react-router'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export const Route = createRootRoute({
   component: () => (
     <main className="h-screen w-screen flex justify-center items-center flex-col">
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-
       <Outlet />
       <TanStackRouterDevtools />
       <ReactQueryDevtools />
--- apps/web/src/routes/_app/home.tsx
+++ apps/web/src/routes/_app/home.tsx
@@ -1,31 +1,119 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { useState } from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+// import { useState } from 'react'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+const formSchema = z.object({
+  name: z.string().min(5).max(50),
+  description: z.string().min(5).max(100),
+})
 
 export const Route = createFileRoute('/_app/home')({
   component: Home,
 })
 
-function urlAddShow({}) {
-  if (ValidURL()) {
-    return (
-      <Button type="submit" onClick={}>
-        New Link
-      </Button>
-    )
-  }
-}
+// function urlAddShow({}) {
+//   if (ValidURL()) {
+//     return (
+//       <Button type="submit" onClick={}>
+//         New Link
+//       </Button>
+//     )
+//   }
+// }
 
 function Home() {
-  const [index, setIndex] = useState(0)
+  // const [index, setIndex] = useState(0)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
   return (
     <div>
-      Hello "/(app)/home"!
       <div className="flex w-full max-w-sm items-center space-x-2">
         <Input type="text" placeholder="New Link or Search" />
-
-        <Button type="submit">New Space</Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button type="submit">New Space</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Wanna create some spacey</DialogTitle>
+              <DialogDescription>
+                Create a new space to share with your friends
+              </DialogDescription>
+            </DialogHeader>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Space Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="some namy" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your space's public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Space Description</FormLabel>
+                      <FormControl>
+                        <Input placeholder="some descriptionnnn" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        wanna explain what this space is about?
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   )
