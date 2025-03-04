151bfc27105ca68d2b25b70c5cd26d180c54a48f
yanyu <yanyu_chen@fis.edu>
2025-03-04 15:09:06 +0100

    better Navbar

--- apps/web/src/components/ui/navigation-menu.tsx
+++ apps/web/src/components/ui/navigation-menu.tsx
@@ -59,7 +59,7 @@ function NavigationMenuItem({
 }
 
 const navigationMenuTriggerStyle = cva(
-  'group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:focus:bg-accent data-[state=open]:bg-accent/50 ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 transition-[color,box-shadow] focus-visible:ring-4 focus-visible:outline-1'
+  'group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-2 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:focus:bg-accent data-[state=open]:bg-accent/50 ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 transition-[color,box-shadow] focus-visible:ring-4 focus-visible:outline-1'
 )
 
 function NavigationMenuTrigger({
@@ -75,7 +75,7 @@ function NavigationMenuTrigger({
     >
       {children}{' '}
       <ChevronDownIcon
-        className="relative top-[1px] ml-1 size-3 transition duration-300 group-data-[state=open]:rotate-180"
+        className="relative top-[1px] ml-0.5 size-3 transition duration-300 group-data-[state=open]:rotate-180"
         aria-hidden="true"
       />
     </NavigationMenuPrimitive.Trigger>
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,10 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Button } from '@/components/ui/button'
 import { useQuery } from '@tanstack/react-query'
-import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu'
 import { Link } from '@tanstack/react-router'
-import * as React from 'react'
-import { cn } from '@/lib/utils'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -15,7 +13,6 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from '@/components/ui/navigation-menu'
-import { buttonVariants } from '@/components/ui/button'
 
 const apiUrl = import.meta.env.VITE_API_URL
 
@@ -32,97 +29,41 @@ function Index() {
     },
   })
 
-  //drop down components
-  const components: { title: string; href: string }[] = [
-    {
-      title: 'Home',
-      href: '/home',
-    },
-    {
-      title: 'Profile',
-      href: '/docs/primitives/hover-card',
-    },
-    {
-      title: 'Settings',
-      href: '/docs/primitives/progress',
-    },
-    {
-      title: 'Logout',
-      href: '/auth/signin',
-    },
-  ]
-
   return (
-    <>
-      <div className="fixed top-0 flex-row flew-grow justify-between items-center p-4 bg-background text-foreground">
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Profile</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[150px] gap-3 p-4">
-                  {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    ></ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink>Link</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link to="/auth/signup">
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Documentation
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+    <body className="flex min-h-screen flex-col overflow-hidden overflow-y-auto scrollbar-thin scrollbar-track-background scrollbar-thumb-accent bg-background">
+      <div className="relative flex flex-col item-center gap-8">
+        <div className="relative flex flex-col w-screen my-2 p-5">
+          <div className="relative flex flex-row p-3 space-x-4 bg-muted text-muted-foreground rounded-xl shadow-md">
+            <div className="relative flex">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="relative flex">
+              <NavigationMenu>
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger>Profile</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <NavigationMenuLink>Link</NavigationMenuLink>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </div>
+          </div>
+        </div>
+        <div className="relative flex flex-col w-screen my-2 p-8 bg-background text-foreground">
+          <h1 className="flex my-10 text-center text-6xl font-extrabold tracking-tight">
+            Share and organize links effortlessly on a single, collaborative hub
+            with Share-It.
+          </h1>
+          <Button size="sm" variant="outline">
+            <Link to="/auth/signup">Sign up Here</Link>
+          </Button>
+        </div>
       </div>
-
-      <div className="flex justify-center items-center flex-grow flex-col space-y-5 p-4 mx-auto max-w-xxl px-4 bg-background text-foreground">
-        <h1 className="flex scroll-m-20 text-4xl font-extrabold tracking-tight text-center py-4 px-50">
-          Share and organize links effortlessly on a single, collaborative hub
-          with Share-It.
-        </h1>
-        <Button size="xl" variant="outline">
-          <Link to="/auth/signup">Sign up Here</Link>
-        </Button>
-      </div>
-    </>
+    </body>
   )
 }
-
-const ListItem = React.forwardRef<
-  React.ElementRef<'a'>,
-  React.ComponentPropsWithoutRef<'a'>
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  )
-})
-ListItem.displayName = 'ListItem'
