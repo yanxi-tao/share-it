2f068ceeeb17168c9a2114891c554b1900ce0672
yanyu <yanyu_chen@fis.edu>
2025-03-05 14:52:24 +0100

    navbar v2

--- apps/web/src/routes/_app/about.tsx
+++ apps/web/src/routes/_app/about.tsx
@@ -1,9 +1,34 @@
 import { createFileRoute } from '@tanstack/react-router'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from '@/components/ui/navigation-menu'
 
 export const Route = createFileRoute('/_app/about')({
   component: About,
 })
 
 function About() {
-  return <div className="p-2">Hello from About!</div>
+  return (
+    <div className="flex min-h-screen flex-col overflow-hidden overflow-y-auto scrollbar-thin scrollbar-track-background scrollbar-thumb-accent bg-background">
+      <div className="relative flex">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Profile</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
+  )
 }
